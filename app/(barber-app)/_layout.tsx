35b4a39131cfb30f3fe7abfad064e0f51631b196
Daniel <daniel.martos@hotmail.com>
2025-05-07 23:44:30 +0200

    Todo anadido

--- app/(barber-app)/_layout.tsx
+++ app/(barber-app)/_layout.tsx
@@ -7,6 +7,7 @@ import { ActivityIndicator, View } from "react-native";
 import useAuthStore from "../auth/store/useAuthStore";
 
 const CheckAuthLayout = () => {
+  //TODO: Dividir por admin, empleado, o cliente
   const { status, checkStatus, user } = useAuthStore();
 
   useEffect(() => {
